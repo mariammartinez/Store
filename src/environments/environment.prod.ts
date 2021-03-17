abbc44c546e0df27ed899c9ab4b1875237078406
mariammartinez <mariammartinez@users.noreply.github.com>
2021-03-17 09:39:15 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  url_api: 'https://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyDj7h_9-LegapLABW0AepjL3Ud5g-tXsgk',
+    authDomain: 'platzi-store-6af08.firebaseapp.com',
+    projectId: 'platzi-store-6af08',
+    storageBucket: 'platzi-store-6af08.appspot.com',
+    messagingSenderId: '909350717895',
+    appId: '1:909350717895:web:280ab635ed84f6fb7f71e2'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url_api: 'https://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyDj7h_9-LegapLABW0AepjL3Ud5g-tXsgk',
+    authDomain: 'platzi-store-6af08.firebaseapp.com',
+    projectId: 'platzi-store-6af08',
+    storageBucket: 'platzi-store-6af08.appspot.com',
+    messagingSenderId: '909350717895',
+    appId: '1:909350717895:web:280ab635ed84f6fb7f71e2'
+  }
 };
 
 /*
